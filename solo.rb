44cc49d5bd6de702c224daba1a1facdd860bbf82
Alex Goodman <wagoodman@gmail.com>
2016-11-06 14:54:14 -0500

    removed chrome and dynamodb for now

--- solo.rb
+++ solo.rb
@@ -2,12 +2,5 @@ file_cache_path  "/my-dev-box/kitchen/"
 cookbook_path    ["/my-dev-box/kitchen/cookbooks", "/my-dev-box/kitchen/site-cookbooks"]
 #    "recipe[dynamodb-local]",
 #    "recipe[dynamodb-local::upstart]"
-#    "recipe[redisio::enable]"
-#    "recipe[rvm::system]",
-#    "recipe[rvm::vagrant]",
+#    "recipe[chrome]"
 #
-#
-#
-#
-#
-#    
